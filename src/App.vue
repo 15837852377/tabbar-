b2061dd5cc01c515a840aff3147978404418d617
15837852377 <616393764@qq.com>
2022-06-28 09:52:58 +0800

    '完成项目初始化'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <my-header></my-header>
+    <my-footer></my-footer>
+  </div>
 </template>
-
 <script>
-export default {}
+import MyFooter from './components/MyFooter.vue'
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  components: { MyHeader, MyFooter },
+  data() {
+    return {
+      tabList: [
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList'
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch'
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style></style>
